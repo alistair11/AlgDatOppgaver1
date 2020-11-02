3420e8b0ad25c08334e77f82d80d9687c1504cd7
Ali Reza <s341823@oslomet.no>
2020-11-02 10:26:43 +0100

    Hello

--- src/uke11/HeapSort.java
+++ src/uke11/HeapSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class HeapSort {
     public static void main(String [] args){
-        int values[] ={-99,5,9,8,3,1};
+        int values[] ={-99,5,9,8,3,1};  // bruker ikke indeks 0
 
 
     }
@@ -20,16 +20,19 @@ public class HeapSort {
 
     }
     static void heapify(int [] values){
-        for (int i=1; i<values.length; i++){
-            int current=i;
-            int parent =i/2;
+        for (int i=1; i<values.length; i++){ // Gjør vi om til et minimumsheap, hopper over indeks 0
+            int current=i;   // iden til indeks 0
+            int parent =i/2;  // forelder noden
 
-            while (parent >0 && values[parent] > values[current]){
+            while (parent >0 && values[parent] > values[current]){  // looper opp
                 System.out.println("Bytter "+current+" med "+parent);
+                // Bytter vi om
                 int temp=values[parent];
                 values[parent]=values[current];
                 values[current]=temp;
+                // Oppdaterer current
                 current=parent;
+                //Oppdaterer current
                 parent=current/2;
 
             }
